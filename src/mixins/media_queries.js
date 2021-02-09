69b86eed6a6be6fdc820da99ff4548c6bcc08700
Philip Tam <philip.tam.code@gmail.com>
2021-02-08 19:29:20 -0500

    Added Media Queries, Fixed Navbar, updated React, Materiaul UI, etc

---
+++ src/mixins/media_queries.js
@@ -0,0 +1,6 @@
+const media_queries = {
+        small: "(max-width: 600px)",
+        medium: "(min-width: 601px) and (max-width: 1199px)",
+        large: "(min-width: 1200px)"
+};
+export default media_queries;
